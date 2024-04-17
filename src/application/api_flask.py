4e8d3378af35effb66b2f1bbaaba44e075629545
hoangphamvcc <hoangphamviet@vccorp.vn>
2024-04-17 08:59:07 +0700

    update maintain hours

--- src/application/api_flask.py
+++ src/application/api_flask.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from src.domain import ApiControl
+from src.common.mongo import MongoDB
 
+mongodb = MongoDB()
 app = Flask(__name__)
 
 
@@ -9,8 +11,9 @@ def api_control_price():
     plan_name = request.json.get('plan_name')
     category_code = request.json.get('category_code')
     quantity = request.json.get('quantity')
-    maintain_hours = request.json.get('maintain_hours')
+    maintain_hours = MongoDB().maintain_hours['maintain_hours']
     try:
+        maintain_hours = request.json.get('maintain_hours')
         api = ApiControl(plan_name, category_code, quantity, maintain_hours)
         return jsonify(api.api_price_output()), 200
     except Exception as e:
--- src/common/mongo.py
+++ src/common/mongo.py
@@ -6,4 +6,4 @@ class MongoDB:
     def __init__(self):
         self.__client = pymongo.MongoClient(MONGO_URI)
         self.db = self.__client[MONGO_DB_NAME]
-        self.news = self.db[MONGO_TASK_1_COLLECTION]
+        self.maintain_hours = self.db[MONGO_TASK_1_COLLECTION]
