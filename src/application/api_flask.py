ab54ae13fdc8a1913c99668fc5bc83030eea5ab3
hoangphamvcc <hoangphamviet@vccorp.vn>
2024-04-19 08:51:37 +0700

    update 3 api

--- src/application/api_flask.py
+++ src/application/api_flask.py
@@ -2,8 +2,18 @@ from flask import Flask, request, jsonify
 from src.domain import ApiControl
 from src.common.mongo import MongoDB
 
-mongodb = MongoDB()
+mongodb = MongoDB().maintain_hours
 app = Flask(__name__)
+
+
+@app.route('/change_api/<string:function>', methods=['POST'])
+def change_api(function):
+    if function == 'add' or function == 'change':
+        plan_name = request.json.get('plan_name')
+        api = ApiControl(plan_name, category_code='dedicated', quantity=1, maintain_hours=1)
+        return jsonify({'amount': api.api_price_output()}), 200
+    elif function == 'delete':
+        return jsonify({'amount': 0}), 200
 
 
 @app.route('/api', methods=['POST'])
@@ -11,14 +21,19 @@ def api_control_price():
     plan_name = request.json.get('plan_name')
     category_code = request.json.get('category_code')
     quantity = request.json.get('quantity')
-    maintain_hours = MongoDB().maintain_hours['maintain_hours']
+    maintain_hours = mongodb['maintain_hours']
     try:
         maintain_hours = request.json.get('maintain_hours')
         api = ApiControl(plan_name, category_code, quantity, maintain_hours)
-        return jsonify(api.api_price_output()), 200
+        return jsonify({'amount': api.api_price_output()}), 200
     except Exception as e:
         return jsonify(str(e)), 400
 
+
+@app.route('/api/maintain_hour', methods=['GET'])
+def get_maintain_hours():
+    maintain_hours = mongodb['maintain_hours']
+    return jsonify({'maintain_hours': maintain_hours}), 200
 
 if __name__ == "__main__":
     app.run(port=8887, debug=True)
--- src/domain/task_demo.py
+++ src/domain/task_demo.py
@@ -8,6 +8,10 @@ class ApiControl:
     headers = {'Content-Type': 'application/json'}
 
     def __init__(self, plan_name, category_code, quantity, maintain_hours):
+        self.plan_name = plan_name
+        self.category_code = category_code
+        self.quantity = quantity
+
         self.__api_output = {
             "plan_name": f"{plan_name}",
             "category_code": f"{category_code}",
